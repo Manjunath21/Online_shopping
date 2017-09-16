bf2ecf2818ec8f2014a5072d75e6cad11e986241
manjunath <manjunath.man3@gmail.com>
2017-09-16 10:48:10 +0530

    my project setup completed successfully.

---
+++ shoppingbackend/src/main/java/com/manju/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.manju.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
